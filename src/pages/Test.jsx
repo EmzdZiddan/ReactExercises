f9dc5301ea6e0f981240e2479b0fc1c489cfb97c
EmzdZiddan <emzdziddan@gmail.com>
2026-01-29 23:13:45 +0700

    test use state and use effect

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,38 +1,26 @@
-import { useState, useEffect } from "react";
+import { useState,useEffect } from "react";
+
 
 const Test = () => {
-    const [pacarSaya, setPacarSaya] = useState(0);
-    const [status, setStatus] = useState('');
-    
-    useEffect(() => {
-        if (pacarSaya >= 1 ) {
-            setStatus('ada');
-        } else {
-            setStatus('tidak ada');
-        }
-    }, [pacarSaya]);
+  const [angka, setAngka] = useState(0);
+  const [status, setStatus] = useState('');
 
+useEffect(() => {
+    if (angka % 2 === 0) {
+        setStatus('Genap');
+    } else {
+        setStatus('Ganjil');
+    }
+})
 
   return (
-    <div className="bg-gray-800 flex flex-col items-center justify-center gap-4 h-screen">
+    <div>
         <h1>{status}</h1>
-
-        <h1 className="text-white text-6xl font-bold">{pacarSaya}</h1>
-        
-        <button
-            onClick={() => setPacarSaya(pacarSaya + 1)}
-            className="bg-green-600 text-lg text-white font-bold rounded-xl px-6 py-3 hover:bg-green-700"
-            >Tambah Pacar
-        </button>
-
-        <button
-            onClick={() => setPacarSaya(pacarSaya - 1)}
-            className="bg-red-600 text-lg text-white font-bold rounded-xl px-6 py-3 hover:bg-red-700 disabled:opacity-50 disabled:cursor-not-allowed"
-            >Kurang Pacar
-        </button>
-
+      <h1>Angka: {angka}</h1>
+      <button onClick={() => setAngka(angka + 1)}>Tambah</button>
     </div>
   );
 };
+
 
 export default Test;
